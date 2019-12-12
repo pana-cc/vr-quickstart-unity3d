2f02fad09ce565b727780cd4146b3e965a4278f9
Panayot Cankov <panayot.cankov@progress.com>
2019-12-12 18:03:09 +0200

    Add reset button and IL2CPP for build for android.

---
+++ Unity/QuickStartVR-Done/Assets/Scripts/Billboard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // this.transform.LookAt(Camera.main.transform.position);
+        this.transform.rotation = Quaternion.LookRotation(this.transform.position - Camera.main.transform.position, Vector3.up);
+    }
+}
---
+++ Unity/QuickStartVR-Done/Assets/Scripts/ResetButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButton : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody ballBig;
+
+    [SerializeField]
+    Rigidbody ballSmall1;
+
+    [SerializeField]
+    Rigidbody ballSmall2;
+
+    [SerializeField]
+    Rigidbody ballSmall3;
+
+    [SerializeField]
+    Collider leftHand;
+
+    [SerializeField]
+    Collider rightHand;
+
+    Vector3 ballBigPosition;
+    Vector3 ballSmall1Position;
+    Vector3 ballSmall2Position;
+    Vector3 ballSmall3Position;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.ballBigPosition = this.ballBig.transform.position;
+        this.ballSmall1Position = this.ballSmall1.transform.position;
+        this.ballSmall2Position = this.ballSmall2.transform.position;
+        this.ballSmall3Position = this.ballSmall3.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == this.leftHand || other == this.rightHand)
+        {
+            this.ballBig.transform.position = this.ballBigPosition;
+            this.ballBig.velocity = Vector3.zero;
+            this.ballBig.angularVelocity = Vector3.zero;
+
+            this.ballSmall1.transform.position = this.ballSmall1Position;
+            this.ballSmall1.velocity = Vector3.zero;
+            this.ballSmall1.angularVelocity = Vector3.zero;
+
+            this.ballSmall2.transform.position = this.ballSmall2Position;
+            this.ballSmall2.velocity = Vector3.zero;
+            this.ballSmall2.angularVelocity = Vector3.zero;
+
+            this.ballSmall3.transform.position = this.ballSmall3Position;
+            this.ballSmall3.velocity = Vector3.zero;
+            this.ballSmall3.angularVelocity = Vector3.zero;
+        }
+    }
+}
